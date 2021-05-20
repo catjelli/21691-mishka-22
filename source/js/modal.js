7158d7151d904eb81deeb63f3b469b738ece45da
CatJelli <catjelli@gmail.com>
2021-05-20 11:21:20 +0300

    готова к получению допуска

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,9 +1,26 @@
 const modalButton = document.querySelector(".week-product__button");
 const modal = document.querySelector(".modal");
 const modalOverlay = document.querySelector(".modal-overlay");
+const modalClose = modal.querySelector(".modal__button");
 
 modalButton.addEventListener("click", function (evt) {
   evt.preventDefault();
   modal.classList.add("modal--show");
   modalOverlay.classList.add("modal-overlay--show");
-}
+});
+
+modalClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modal.classList.remove("modal--show");
+  modalOverlay.classList.remove("modal-overlay--show");
+});
+
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode === 27) {
+    if (modal.classList.contains("modal--show")) {
+      evt.preventDefault();
+      modal.classList.remove("modal--show");
+      modalOverlay.classList.remove("modal-overlay--show");
+    }
+  }
+});
--- source/js/script.js
+++ source/js/script.js
@@ -1,10 +1,6 @@
 let navMain = document.querySelector(".main-nav");
 let navToggle = document.querySelector(".nav-toggle");
 let siteNav = document.querySelector(".site-nav");
-const modalButton = document.querySelector(".week-product__button");
-const modal = document.querySelector(".modal");
-const modalOverlay = document.querySelector(".modal-overlay");
-const modalClose = modal.querySelector(".modal__button");
 
 navMain.classList.remove("main-nav--nojs");
 navToggle.classList.remove("nav-toggle--nojs");
@@ -21,27 +17,5 @@ navToggle.addEventListener("click", function() {
     navMain.classList.remove("main-nav--opened");
     navToggle.classList.add("nav-toggle--closed");
     navToggle.classList.remove("nav-toggle--opened");
-  }
-});
-
-modalButton.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  modal.classList.add("modal--show");
-  modalOverlay.classList.add("modal-overlay--show");
-});
-
-modalClose.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  modal.classList.remove("modal--show");
-  modalOverlay.classList.remove("modal-overlay--show");
-});
-
-window.addEventListener("keydown", function(evt) {
-  if (evt.keyCode === 27) {
-    if (modal.classList.contains("modal--show")) {
-      evt.preventDefault();
-      modal.classList.remove("modal--show");
-      modalOverlay.classList.remove("modal-overlay--show");
-    }
   }
 });
